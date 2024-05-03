88a7db19356ba8b14fa7f2ee1cf69531c3c09244
Cabr1to <luis.b.c.melo@gmail.com>
2024-05-03 16:54:02 -0300

    ok file add

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -1,6 +1,14 @@
 function Subscribe () {
     var subs = document.getElementById('subscribe');
 
-
-
 }
+
+var email = document.getElementById("email");
+
+email.addEventListener('focus', ()=>{
+    email.style.borderColor = "hsl(240,1%,17%)";
+});
+
+email.addEventListener('blur', ()=>{
+    email.style.borderColor = "#ccc";
+});
